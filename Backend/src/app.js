13f048500ce4e148226ca55ad0f87bba284dd1a2
Azmi <aji11072016@gmail.com>
2026-01-09 08:55:15 +0700

    Berhasil Tes sampai postman

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const app = express();
 
+const authRoute = require('./routes/authrute');
 //Basic Middleware
 app.use(express.json());
+
+//rute Middleware
+app.use('/api/auth', authRoute);
 
 //Endpoint Test
 app.get('/health', (req, res) => {
--- Backend/src/server.js
+++ Backend/src/server.js
@@ -1,11 +1,18 @@
 require('dotenv').config();
 
+const app = require('./app');
 const sequelize = require('./config/database');
 
+const PORT = process.env.PORT || 3000;
 (async () => {
   try {
     await sequelize.authenticate();
     console.log('Database connected');
+
+    app.listen(PORT, () => {
+      console.log(`Server is running on port http://localhost:${PORT}`);
+      console.log(`Health check at: http://localhost:${PORT}/health`);  
+    });
   } catch (error) {
     console.error('DB error:', error.message);
   }
